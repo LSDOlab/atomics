e2479e642b6e4d0a0cc992865b1bb36ff6ad6483
JYan_Linux <jiy352@ucsd.edu>
2020-12-04 11:32:54 -0800

    update

--- atomics/examples/run_battery_paper_mix_quarter.py
+++ atomics/examples/run_battery_paper_mix_quarter.py
@@ -53,11 +53,16 @@ q = df.Constant((POWER/AREA_CYLINDER)) # bdry heat flux
 # constants for thermoelastic model
 K = 69e9
 ALPHA = 13e-6
-f_l = df.Constant(( 1.e6/AREA_SIDE, 0.)) 
-f_r = df.Constant((-1.e6/AREA_SIDE, 0.)) 
-f_b = df.Constant(( 0.,  1.e6/AREA_SIDE)) 
-f_t = df.Constant(( 0., -1.e6/AREA_SIDE))
+f_l = df.Constant(( 1.e6/AREA_SIDE/2, 0.)) 
+f_r = df.Constant((-1.e6/AREA_SIDE/2, 0.)) 
+f_b = df.Constant(( 0.,  1.e6/AREA_SIDE/2)) 
+f_t = df.Constant(( 0., -1.e6/AREA_SIDE/2))
 
+
+# f_l = df.Constant(( 0., 0.)) 
+# f_r = df.Constant((0., 0.)) 
+# f_b = df.Constant(( 0.,  0.)) 
+# f_t = df.Constant(( 0., 0.))
 '''
 2. Define mesh
 '''
@@ -75,7 +80,10 @@ with pygmsh.occ.Geometry() as geom:
         disk_dic[name] = geom.add_disk([xv.flatten()[i], yv.flatten()[i], 0.], 0.01)
         disks.append(disk_dic[name])
 
-    geom.boolean_difference(rectangle, geom.boolean_union(disks))
+    rectangle_1 = geom.add_rectangle([START_X, START_Y, 0.], LENGTH, WIDTH/2)
+    rectangle_2 = geom.add_rectangle([START_X, 0., 0.], LENGTH/2, WIDTH/2)
+    geom.boolean_difference(rectangle, geom.boolean_union([disks, rectangle_1, rectangle_2]))
+
 
     mesh = geom.generate_mesh()
     mesh.write("test_2d.vtk")
@@ -100,9 +108,9 @@ os.system('gmsh -2 test_2d.vtk -format msh2')
 os.system('dolfin-convert test_2d.msh mesh_2d.xml')
 mesh = df.Mesh("mesh_2d.xml")
 
-# import matplotlib.pyplot as plt
-# df.plot(mesh)
-# plt.show()
+import matplotlib.pyplot as plt
+df.plot(mesh)
+plt.show()
 
 '''
 3. Define traction bc subdomains
@@ -120,9 +128,10 @@ class HeatBoundary(df.SubDomain):
 #-----------define-surrounding-heat-sink-boundary-------
 class SurroundingBoundary(df.SubDomain):
     def inside(self, x, on_boundary):
-        return ( abs(x[0] -   START_X)  < df.DOLFIN_EPS or
+        return ( 
+                # abs(x[0] -   START_X)  < df.DOLFIN_EPS or
                 abs(x[0] - (-START_X)) < df.DOLFIN_EPS or  
-                abs(x[1] -   START_Y)  < df.DOLFIN_EPS or
+                # abs(x[1] -   START_Y)  < df.DOLFIN_EPS or
                 abs(x[1] - (-START_Y)) < df.DOLFIN_EPS)
 
 # Mark the HeatBoundary ass dss(6)
@@ -130,9 +139,12 @@ sub_domains = df.MeshFunction('size_t', mesh, mesh.topology().dim() - 1)
 heat_edge = HeatBoundary()
 heat_edge.mark(sub_domains, 6)
 
-class LeftBoundary(df.SubDomain):
+class MidHBoundary(df.SubDomain):
     def inside(self, x, on_boundary):
-        return (abs(x[0] - START_X)< df.DOLFIN_EPS)
+        return (abs(x[1] )< df.DOLFIN_EPS)
+class MidVBoundary(df.SubDomain):
+    def inside(self, x, on_boundary):
+        return (abs(x[0] )< df.DOLFIN_EPS)
 
 class RightBoundary(df.SubDomain):
     def inside(self, x, on_boundary):
@@ -152,13 +164,13 @@ class CenterBoundary(df.SubDomain):
 
 # Mark the traction boundaries 8 10 12 14
 # sub_domains = df.MeshFunction('size_t', mesh, mesh.topology().dim() - 1)
-left_edge  = LeftBoundary()
+# left_edge  = LeftBoundary()
 right_edge = RightBoundary()
-bottom_edge = BottomBoundary()
+# bottom_edge = BottomBoundary()
 top_edge = TopBoundary()
-left_edge.mark(sub_domains, 8)
+# left_edge.mark(sub_domains, 8)
 right_edge.mark(sub_domains, 10)
-bottom_edge.mark(sub_domains, 12)
+# bottom_edge.mark(sub_domains, 12)
 top_edge.mark(sub_domains, 14)
 
 dss = df.Measure('ds')(subdomain_data=sub_domains)
@@ -207,8 +219,7 @@ residual_form = get_residual_form(
     ALPHA
 )
 
-residual_form -= ( df.dot(f_l, v) * dss(8) + df.dot(f_r, v) * dss(10) + df.dot(f_b, v) * dss(12) 
-                    + df.dot(f_t, v) * dss(14) + q*T_hat*dss(6) )
+residual_form -=  (df.dot(f_r, v) * dss(10) + df.dot(f_t, v) * dss(14)  + q*T_hat*dss(6) )
 print("get residual_form-------")
 pde_problem.add_state('mixed_states', mixed_function, residual_form, 'density')
 
@@ -224,8 +235,8 @@ print("Add output-avg_density-------")
 
 # Add output-compliance to the PDE problem:
 
-compliance_form = df.dot(f_l, displacements_function) * dss(8) +df.dot(f_r, displacements_function) * dss(10) +\
-                    df.dot(f_b, displacements_function) * dss(12) + df.dot(f_t, displacements_function) * dss(14)
+compliance_form = df.dot(f_r, displacements_function) * dss(10) +\
+                    df.dot(f_t, displacements_function) * dss(14) 
 pde_problem.add_scalar_output('compliance', compliance_form, 'mixed_states')
 print("Add output-compliance-------")
 
@@ -234,8 +245,8 @@ print("Add output-compliance-------")
 4. 3. Add bcs
 '''
 
-bc_displacements = df.DirichletBC(mixed_fs.sub(0).sub(0), df.Constant((0.0)), CenterBoundary())
-bc_displacements_1 = df.DirichletBC(mixed_fs.sub(0).sub(1), df.Constant((0.0)), CenterBoundary())
+bc_displacements = df.DirichletBC(mixed_fs.sub(0).sub(0), df.Constant((0.0)), MidVBoundary())
+bc_displacements_1 = df.DirichletBC(mixed_fs.sub(0).sub(1), df.Constant((0.0)), MidHBoundary())
 
 bc_temperature = df.DirichletBC(mixed_fs.sub(1), df.Constant(T_0), SurroundingBoundary())
 
@@ -289,7 +300,7 @@ prob.run_model()
 # prob.check_partials(compact_print=True)
 # print(prob['compliance']); exit()
 
-prob.run_driver()
+# prob.run_driver()
 
 displacements_function_val, temperature_function_val= mixed_function.split()
 
